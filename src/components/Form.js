1cf5abbd298f2eec656fdcd148274f92c792adc1
Benjamin Dean <benjamin_h_dean@yahoo.com>
2021-02-11 11:29:22 +0100

    form again

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
-import webWords from '../assets/web-words.png'
+import twitter from '../assets/social-network-icon_twitter.png'
+import linkedin from '../assets/social-network-icon_linkedin.png'
+import github from '../assets/social-network-icon_github.png'
 
 
 export default function ContactUs() {
@@ -47,23 +49,46 @@ export default function ContactUs() {
 
             <div className="slider2"></div>
 
+            <div className="three-columns">
+                <div className="contact-prez" >
+                    <h2 style={{ marginBottom: "28px" }}>DEAN DESIGN</h2>
+                    <h3>Benjamin H. Dean</h3>
+                    <p>Web/UI/UX Visual Designer</p>
+                </div>
+                <div className="contact-text">
+                    <p style={{ lineHeight: "28px", marginTop: "5px" }}>Vous souhaitez concevoir, créer, refondre, développer et optimiser les parcours utilisateurs, l’ergonomie, l’expérience et les interfaces pour votre client ? Accédez à mes services créatifs et techniques. Contactez-moi par message ou mobile.</p>
+                </div>
+                <div>
+                    <div className="two-icons">
+                        <a href="https://www.linkedin.com/in/benjamin-hudson-dean/" target="_blank" rel="noreferrer"><img src={linkedin} alt="LinkedIn" height="" width="" className="contact-social-icon" /></a>
+                        <a href="https://github.com/bdean05" target="_blank" rel="noreferrer"><img src={github} alt="GitHub" height="" width="" className="contact-social-icon" /></a>
+                        <a href="https://twitter.com/benjamin_h_dean" target="_blank" rel="noreferrer"><img src={twitter} alt="Twitter" height="" width="" className="contact-social-icon" /></a>
+                    </div>
 
-            <div className="blocks">
-                <div className="block">
-                    <p>Vous souhaitez concevoir, créer, refondre, développer et optimiser les parcours utilisateurs, l’ergonomie, l’expérience et les interfaces pour votre client ? Accédez à mes services créatifs et techniques. N'hésitez pas à télécharger mon CV et mon portfolio.</p>
+                    <div >
+                        <p className="contact-link"><a href="mailto:benjamin_h_dean@yahoo.com" style={{ padding: "0px 0px 0px 20px" }}>E-mail : cliquez ici (inbox)</a></p>
+                        <p className="mycontact">Mob : (+33) 6 14 31 73 56</p>
+                        <p className="mycontact">Paris, France</p>
+                    </div>
+
                 </div>
             </div>
-            <form className="contact-form" onSubmit={sendEmail}>
-                <div><input type="hidden" name="contact_number" /></div>
-                <div><label>Name</label></div>
-                <div><input type="text" name="user_name" value={name} onChange={e => setName(e.target.value)} required /></div>
-                <div><label>Email</label></div>
-                <div><input type="email" name="user_email" value={email} onChange={e => setEmail(e.target.value)} required /></div>
-                <div><label>Message</label></div>
-                <div><textarea name="message" value={message} onChange={e => setMessage(e.target.value)} required /></div>
-                <div><input type="submit" value="Send" disabled={loading === true} /></div>
-            </form>
-
+            <div className="contact-form" >
+                <form onSubmit={sendEmail}>
+                    <div><input type="hidden" name="contact_number" /></div>
+                    <div><label>Nom</label></div>
+                    <div><input type="text" name="user_name" value={name} onChange={e => setName(e.target.value)} required /></div>
+                    <div><label>E-mail</label></div>
+                    <div><input type="email" name="user_email" value={email} onChange={e => setEmail(e.target.value)} required /></div>
+                    <div><label>Message</label></div>
+                    <div><textarea name="message" value={message} onChange={e => setMessage(e.target.value)} required /></div>
+                    <div><input type="submit" value="Envoyer" disabled={loading === true} /></div>
+                </form>
+                <div className="extra-contact-box">
+                    <p>Mob : (+33) 6 14 31 73 56</p>
+                    <p>Paris, France</p>
+                </div>
+            </div>
         </div>
     );
 }
