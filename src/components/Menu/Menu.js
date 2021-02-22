5288013b4a714501d1dea58764fec1b006d2889a
Benjamin Dean <benjamin_h_dean@yahoo.com>
2021-02-22 19:48:08 +0100

    gyrd

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -10,10 +10,10 @@ const Menu = ({ open }) => {
     return (
         <StyledMenu open={open}>
             <a href="/" >Accueil</a>
-            <a href="/#a-propos" style={{ marginTop: "-24px" }}>À propos</a>
-            <a href="/portfolio" style={{ marginTop: "-24px" }}>Portfolio</a>
-            <a href="/javareact" style={{ marginTop: "-24px" }}>React</a>
-            <a href="/form" style={{ marginTop: "-24px" }}>Contact</a>
+            <a href="/#a-propos" style={{ marginTop: "-26px" }}>À propos</a>
+            <a href="/portfolio" style={{ marginTop: "-26px" }}>Portfolio</a>
+            <a href="/javareact" style={{ marginTop: "-26px" }}>React</a>
+            <a href="/form" style={{ marginTop: "-26px" }}>Contact</a>
             <div style={{ textAlign: "center", marginTop: "10px" }}>
                 <a href="https://www.linkedin.com/in/benjamin-hudson-dean/" target="_blank"><img src={linkedinjpg} alt="LinkedIn" height="" width="30" style={{ padding: "0px 18px 0px 0px" }} /></a>
                 <a href="https://github.com/bdean05" target="_blank"><img src={githubjpg} alt="GitHub" height="" width="30" style={{ padding: "0px 18px 0px 18px" }} /></a>
